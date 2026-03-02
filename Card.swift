8111e9f6d8279fb84f030fd6063ef67fbbf24aa0
John Bradshaw <124829389+Red-Rockwell@users.noreply.github.com>
2026-03-02 22:49:48 +0000

    Files
    Manually added files.

---
+++ Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by John Bradshaw on 22/01/2026.
+//
+
+import Foundation
+
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who was the 3rd Actor to play Batman in the Batman movies?", answer: "George Clooney")
+    
+}
---
+++ FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by John Bradshaw on 19/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
